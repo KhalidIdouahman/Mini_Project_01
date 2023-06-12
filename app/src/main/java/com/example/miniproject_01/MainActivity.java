f293150255d9edf39a3b63a0a2bfaa2fd85a4005
khalid <khalidcourses2022@gmail.com>
2023-06-12 23:19:08 +0100

    Display the full names from a json file in a Toast

--- app/src/main/java/com/example/miniproject_01/MainActivity.java
+++ app/src/main/java/com/example/miniproject_01/MainActivity.java
@@ -8,6 +8,13 @@ import android.widget.Toast;
 
 import com.example.miniproject_01.databinding.ActivityMainBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMainBinding bindingViews;
@@ -27,10 +34,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == bindingViews.loadBtn.getId()) {
-            Toast.makeText(this, "Load btn clicked !!", Toast.LENGTH_SHORT).show();
-        } else  {
+            try {
+                InputStream inputStream = getAssets().open("users.json");
+                int code;
+                StringBuilder contentOfFile = new StringBuilder();
 
-            Toast.makeText(this, "Quit btn clicked !!", Toast.LENGTH_SHORT).show();
+                while ((code = inputStream.read()) != -1) {
+                    contentOfFile.append((char)code);
+                }
+
+                JSONObject mainObj = new JSONObject(contentOfFile.toString());
+                JSONArray users = mainObj.getJSONArray("users");
+                String fullName = "";
+                for (int i = 0; i < users.length(); i++) {
+                    JSONObject user = users.getJSONObject(i);
+                    JSONObject name = user.getJSONObject("name");
+                    fullName += String.format("%s %s \n" , name.get("first") , name.get("last"));
+                }
+                Toast.makeText(this, fullName, Toast.LENGTH_SHORT).show();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        } else  {
+            finish();
         }
     }
 }
