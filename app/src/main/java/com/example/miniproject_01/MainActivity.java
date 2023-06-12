f0268a52a9bb73d09e2cfab67e3195eb721a955b
khalid <khalidcourses2022@gmail.com>
2023-06-13 00:22:16 +0100

    Display the users in an alert dialog depends on they're gender

--- app/src/main/java/com/example/miniproject_01/MainActivity.java
+++ app/src/main/java/com/example/miniproject_01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.miniproject_01;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -45,13 +47,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 JSONObject mainObj = new JSONObject(contentOfFile.toString());
                 JSONArray users = mainObj.getJSONArray("users");
-                String fullName = "";
+
+                ArrayList<JSONObject> maleUsers = new ArrayList<>();
+                ArrayList<JSONObject> femaleUsers = new ArrayList<>();
                 for (int i = 0; i < users.length(); i++) {
                     JSONObject user = users.getJSONObject(i);
-                    JSONObject name = user.getJSONObject("name");
-                    fullName += String.format("%s %s \n" , name.get("first") , name.get("last"));
+                    if (user.getString("gender").equals("male")) {
+                       maleUsers.add(user);
+                    } else {
+                        femaleUsers.add(user);
+                    }
                 }
-                Toast.makeText(this, fullName, Toast.LENGTH_SHORT).show();
+
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                String userInfos = "";
+
+                if (bindingViews.malesRadioBtn.isChecked()) {
+                    for (int i = 0; i < maleUsers.size(); i++) {
+                        JSONObject maleUser = maleUsers.get(i);
+                        JSONObject name = maleUser.getJSONObject("name");
+                        userInfos += String.format("%s %s | %s \n" , name.get("first") , name.get("last") , maleUser.getString("city"));
+                    }
+                    alert.setTitle("Male Users").setMessage(userInfos).show();
+                } else {
+                    for (int i = 0; i < femaleUsers.size(); i++) {
+                        JSONObject femaleUser = femaleUsers.get(i);
+                        JSONObject name = femaleUser.getJSONObject("name");
+                        userInfos += String.format("%s %s | %s \n" , name.get("first") , name.get("last") , femaleUser.getString("city"));
+                    }
+                    alert.setTitle("Female Users").setMessage(userInfos).show();
+
+                }
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
