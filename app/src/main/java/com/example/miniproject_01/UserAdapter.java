d4538a7d47f55ff627db8d1ebc32d1c5d7fee207
khalid <khalidcourses2022@gmail.com>
2023-06-13 12:58:22 +0100

    Display the users in a custom listView + adding a check when we do double click .

--- app/src/main/java/com/example/miniproject_01/UserAdapter.java
+++ app/src/main/java/com/example/miniproject_01/UserAdapter.java
@@ -3,11 +3,14 @@ package com.example.miniproject_01;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 
@@ -16,8 +19,7 @@ import java.util.ArrayList;
 public class UserAdapter extends BaseAdapter {
     ArrayList<UserModel> usersList ;
     Context context;
-//    LayoutInflater inflater;
-
+    private final int DURATION_OF_DOUBLE_CLICK = 250;
     public UserAdapter( Context context , ArrayList<UserModel> usersList) {
         this.usersList = usersList;
         this.context = context;
@@ -46,17 +48,37 @@ public class UserAdapter extends BaseAdapter {
 
         TextView userFullName = convertView.findViewById(R.id.user_full_name_tv);
         TextView userCity = convertView.findViewById(R.id.user_city_tv);
-        Button userDetails = convertView.findViewById(R.id.user_details_btn);
+        ImageView userCheckImg = convertView.findViewById(R.id.user_check_im);
 
         userFullName.setText(user.fullName());
         userCity.setText(user.getCity());
-        userDetails.setOnClickListener(v -> {
-            AlertDialog.Builder alert = new AlertDialog.Builder(context);
-            alert.setTitle("Details of User " + (position + 1))
-                    .setMessage(user.toString())
-                    .show();
-        });
 
+        convertView.setOnTouchListener(new View.OnTouchListener() {
+            long firstClick = 0;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+            //  This is just for if I want to work in multiple action
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_UP:
+//                        Toast.makeText(context, "action up", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case MotionEvent.ACTION_DOWN:
+//                        Toast.makeText(context, "action down", Toast.LENGTH_SHORT).show();
+//                        break;
+//            }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    long secondClick = System.currentTimeMillis();
+
+                    if ((secondClick - firstClick) <= DURATION_OF_DOUBLE_CLICK) {
+                       userCheckImg.setVisibility(userCheckImg.getVisibility() == View.INVISIBLE ? View.VISIBLE : View.INVISIBLE);
+                    } else {
+                        firstClick = secondClick;
+                    }
+
+                }
+                return true;
+            }
+        });
 
         return convertView;
     }
