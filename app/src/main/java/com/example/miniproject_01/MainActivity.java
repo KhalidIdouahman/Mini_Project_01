16c42e775aab01288304fc02b72a9f2df4c0f2a6
khalid <khalidcourses2022@gmail.com>
2023-06-13 21:51:14 +0100

    Load data in AsyncTask .

--- app/src/main/java/com/example/miniproject_01/MainActivity.java
+++ app/src/main/java/com/example/miniproject_01/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.miniproject_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +22,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMainBinding bindingViews;
     View root;
+    LoadingData asyncLoadingData;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -34,11 +38,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         root = bindingViews.getRoot();
         setContentView(root);
 
+        asyncLoadingData = new LoadingData();
+
         bindingViews.loadBtn.setOnClickListener(this);
         bindingViews.userSwipeTv.setOnTouchListener(new MyOnSwipeListener(this) {
             @Override
             public void swipeLeft() {
                 finish();
+                asyncLoadingData.cancel(true);
             }
         });
         //region i try all the lines of code when the swipe didn't work
@@ -61,45 +68,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == bindingViews.loadBtn.getId()) {
-            String data = getDataFromJson("users.json");
-            ArrayList<UserModel> usersInfos = getUsers(data);
-            UserAdapter dataOfListView = new UserAdapter(this , usersInfos);
+            ArrayList<UserModel> usersInfos = null;
+            try {
+                usersInfos = asyncLoadingData.execute("users.json").get();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            UserAdapter dataOfListView = new UserAdapter(this, usersInfos);
             bindingViews.usersListView.setAdapter(dataOfListView);
-        } else  {
-            finish();
         }
     }
 
-    public String getDataFromJson(String fileName) {
-        StringBuilder contentOfFile = new StringBuilder();;
-        try {
-            InputStream inputStream = getAssets().open(fileName);
-            int code;
-            while ((code = inputStream.read()) != -1) {
-                contentOfFile.append((char)code);
-            }
+    public class LoadingData extends AsyncTask<String, Void, ArrayList<UserModel>> {
+        ProgressBar bar;
 
-        } catch (IOException e ) {
-            e.printStackTrace();
-        }
-        return contentOfFile.toString();
-    }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            bindingViews.loadBtn.setEnabled(false);
 
-    public ArrayList<UserModel> getUsers(String jsonContent) {
-        ArrayList<UserModel> usersList = new ArrayList<>();
-        try {
-            JSONObject mainObj = new JSONObject(jsonContent);
-            JSONArray users = mainObj.getJSONArray("users");
-            for (int i = 0; i < users.length(); i++) {
-                JSONObject user = users.getJSONObject(i);
-                JSONObject name = user.getJSONObject("name");
-                usersList.add(new UserModel(name.getString("first") , name.getString("last") , user.getString("gender"),
-                user.getString("city")));
-            }
-//            Toast.makeText(this, fullName, Toast.LENGTH_SHORT).show();
-        } catch (JSONException e) {
-            e.printStackTrace();
+            bar = new ProgressBar(MainActivity.this);
+            Toast.makeText(MainActivity.this, "onPreExecute", Toast.LENGTH_SHORT).show();
+            bar.setActivated(true);
+            bar.setVisibility(View.VISIBLE);
         }
-        return usersList;
+
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+//            Toast.makeText(MainActivity.this, "doInBackground", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected ArrayList<UserModel> doInBackground(String... strings) {
+            ArrayList<UserModel> usersList = new ArrayList<>();
+            String dataFromJsonFile = getDataFromJson(strings[0]);
+            try {
+                JSONObject mainObj = new JSONObject(dataFromJsonFile);
+                JSONArray users = mainObj.getJSONArray("users");
+                for (int i = 0; i < users.length(); i++) {
+                    if (!isCancelled()) {
+                        JSONObject user = users.getJSONObject(i);
+                        Thread.sleep(150);
+                        JSONObject name = user.getJSONObject("name");
+                        usersList.add(new UserModel(name.getString("first"), name.getString("last"),
+                                user.getString("gender"), user.getString("city")));
+                    } else {
+                        return null;
+                    }
+                }
+            } catch (JSONException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            return usersList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<UserModel> userModels) {
+            super.onPostExecute(userModels);
+//            Toast.makeText(MainActivity.this, "onPostExecute", Toast.LENGTH_SHORT).show();
+            bindingViews.loadBtn.setEnabled(true);
+            bar.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        protected void onCancelled(ArrayList<UserModel> userModels) {
+            super.onCancelled(userModels);
+//            Toast.makeText(MainActivity.this, "onCancelled", Toast.LENGTH_SHORT).show();
+        }
+
+        private String getDataFromJson(String fileName) {
+            StringBuilder contentOfFile = new StringBuilder();
+            ;
+            try {
+                InputStream inputStream = getAssets().open(fileName);
+                int code;
+                while ((code = inputStream.read()) != -1) {
+                    contentOfFile.append((char) code);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return contentOfFile.toString();
+        }
+
+//        private ArrayList<UserModel> getUsers(String jsonContent) {
+//            ArrayList<UserModel> usersList = new ArrayList<>();
+//            try {
+//                JSONObject mainObj = new JSONObject(jsonContent);
+//                JSONArray users = mainObj.getJSONArray("users");
+//                for (int i = 0; i < users.length(); i++) {
+//                    JSONObject user = users.getJSONObject(i);
+//                    Thread.sleep(200);
+//                    JSONObject name = user.getJSONObject("name");
+//                    usersList.add(new UserModel(name.getString("first"), name.getString("last"), user.getString("gender"),
+//                            user.getString("city")));
+//                }
+////            Toast.makeText(this, fullName, Toast.LENGTH_SHORT).show();
+//            } catch (JSONException | InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return usersList;
+//        }
     }
 }
